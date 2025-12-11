9e9e662e8dc6304e585de33e295f62137fd0aa06
Matheus Henrique Schumliak <matheeus.hs@gmail.com>
2025-12-11 01:24:04 -0300

    Adiciona suporte a temas e página de login
    - Atualiza `.gitignore` para ignorar arquivos sensíveis e desnecessários.
    - Adiciona suporte a temas claro/escuro com variáveis CSS e lógica em `site.js`.
    - Cria `AccountController` e view `Login.cshtml` para página de login.
    - Altera `_Header.cshtml` para incluir link de login e botão de troca de tema.
    - Ajusta `_Layout.cshtml` para corrigir referência ao `_Footer` e definir idioma como `pt-br`.
    - Melhora estilos em `site.css` para responsividade e consistência entre temas.

---
+++ HoraCerta/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HoraCerta.Controllers
+{
+	public class AccountController : Controller
+	{
+		public AccountController()
+		{
+
+		}
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+	}
+}
